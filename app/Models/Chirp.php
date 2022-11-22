3faf8b2b9e24e4ec0c182e5e13890e93aace121e
Arix Wap <arix.wap@gmail.com>
2022-11-22 18:08:30 +0800

    feat: create chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
