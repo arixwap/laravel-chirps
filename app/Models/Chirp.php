e23ce1dba87033788b8b67b1d0f00ef2839b737d
Arix Wap <arix.wap@gmail.com>
2023-03-02 23:27:04 +0800

    feat: delete chirp and tidyup

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
@@ -18,7 +19,7 @@ class Chirp extends Model
         'message',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
